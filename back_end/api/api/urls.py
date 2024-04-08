9cdbd4d90aa1e9768f682c9d91ed7dfc0b2312ad
Manuelly1 <manuellyvictor2000@gmail.com>
2024-04-08 20:08:42 -0300

    update nos arquivos #50

--- back_end/api/api/urls.py
+++ back_end/api/api/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from core.views import Caixa_DiaCaixaViewSet, PagamentoViewSet
+from core.views import  ClienteViewSet, FornecedorViewSet, FuncionarioViewSet, Caixa_DiaCaixaViewSet, PagamentoViewSet
 
 router = routers.DefaultRouter()
+router.register(r'clientes', ClienteViewSet)
+router.register(r'fornecedores', FornecedorViewSet)
+router.register(r'funcionarios', FuncionarioViewSet)
 router.register(r'caixasDiasCaixas', Caixa_DiaCaixaViewSet)
 router.register(r'pagamentos', PagamentoViewSet)
 
--- back_end/api/core/views.py
+++ back_end/api/core/views.py
@@ -1,8 +1,20 @@
 from rest_framework import viewsets
-from .models import Caixa_DiaCaixa, Pagamento
-from .serializers import CaixaDiaCaixaSerializer, PagamentoSerializer
+from .models import Cliente, Fornecedor, Funcionario, Caixa_DiaCaixa, Pagamento
+from .serializers import ClienteSerializer, FornecedorSerializer, FuncionarioSerializer, CaixaDiaCaixaSerializer, PagamentoSerializer
 
 # Create your views here.
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+
+class FornecedorViewSet(viewsets.ModelViewSet):
+    queryset = Fornecedor.objects.all()
+    serializer_class = FornecedorSerializer
+
+class FuncionarioViewSet(viewsets.ModelViewSet):
+    queryset = Funcionario.objects.all()
+    serializer_class = FuncionarioSerializer
+    
 class Caixa_DiaCaixaViewSet(viewsets.ModelViewSet):
     queryset = Caixa_DiaCaixa.objects.all()
     serializer_class = CaixaDiaCaixaSerializer
