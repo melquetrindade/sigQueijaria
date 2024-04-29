22872c4c3f80cbbc8197effb6e13bab8196cffff
Manuelly1 <manuellyvictor2000@gmail.com>
2024-04-29 10:32:00 -0300

    update

--- front_end/pages/index.js
+++ front_end/pages/index.js
@@ -1,16 +1,11 @@
 import React from "react";
 import styles from '../styles/index.module.css'
-import Link from "next/link";
 
 export default function Home(){
     return(
-        <div>
-            <main className={styles.main}>
-                <h1>SigQueijaria - Página Inicial</h1>
-                <Link href="/Cliente/create">
-                    <button>Inserir Cliente</button>
-                </Link>
-            </main>
-        </div>
+        <main className={styles.main}>
+            <h1>SigQueijaria - Página Inicial</h1>
+        </main>
+       
     )
 }
