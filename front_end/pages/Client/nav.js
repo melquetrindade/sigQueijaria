b2464a08e0eaa17111ff0ad2b3b298adb462997f
Manuelly1 <manuellyvictor2000@gmail.com>
2024-04-30 09:14:37 -0300

    desenvolvimento de uma nav temporária do crud cliente #69

---
+++ front_end/pages/Client/nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function Nav() {
+    return (
+        <nav>
+            <h2>Navegação temporária até aplicar a estilização do CRUD - Cliente</h2>
+            <div>
+                <Link href="create" passHref>
+                    Inserir Cliente
+                </Link>
+            </div>
+            <div>
+                <Link href="read" passHref>
+                    Consultar Cliente
+                </Link>
+            </div>
+        </nav>
+    );
+}
