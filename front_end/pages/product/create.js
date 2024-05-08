4789cd8d26dd2d7f880d5996cc465f4e74ec7417
ErickBezerrar <erickbrtrindade@gmail.com>
2024-05-08 19:49:39 -0300

    Create Complete

--- front_end/pages/product/create.js
+++ front_end/pages/product/create.js
@@ -2,17 +2,19 @@ import React, { useState } from 'react';
 
 export default function AddProduto () {
     const [produto, setProduto] = useState({
-        cpf: '',
-        rg: '',
+        codigoBarras: '',
         nome: '',
-        email: '',
-        numTelefone: '',
-        dataNascimento: ''
+        tipo: '',
+        dataValidade: '',
+        qtdMinima: '',
+        quantidade: '',
+        valor: '',
+        status: ''
     });
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setCliente({ ...cliente, [name]: value });
+        setProduto({ ...produto, [name]: value });
     };
 
     const handleSubmit = async (e) => { // esta função envia os dados do cliente para o back usando o método POST    
@@ -24,19 +26,21 @@ export default function AddProduto () {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(cliente),
+                body: JSON.stringify(produto),
             });
     
             const data = await response.json(); // transforma a resposta em JSON
             console.log(data); 
     
-            setCliente({
-                cpf: '',
-                rg: '',
+            setProduto({
+                codigoBarras: '',
                 nome: '',
-                email: '',
-                numTelefone: '',
-                dataNascimento: ''
+                tipo: '',
+                dataValidade: '',
+                qtdMinima: '',
+                quantidade: '',
+                valor: '',
+                status: ''
             });
         } catch (error) {
             console.error('Erro ao inserir produto!', error);
@@ -45,33 +49,41 @@ export default function AddProduto () {
     
     return (
         <div>
-        <h2>Inserir Cliente</h2>
+        <h2>Inserir Produto</h2>
         <form onSubmit={handleSubmit}>
             <div>
                 <label>CPF:</label>
-                <input type="text" name="cpf" value={cliente.cpf} onChange={handleChange} />
+                <input type="text" name="codigoBarras" value={produto.codigoBarras} onChange={handleChange} />
             </div>
             <div>
                 <label>RG:</label>
-                <input type="text" name="rg" value={cliente.rg} onChange={handleChange} />
+                <input type="text" name="nome" value={produto.nome} onChange={handleChange} />
             </div>
             <div>
                 <label>Nome:</label>
-                <input type="text" name="nome" value={cliente.nome} onChange={handleChange} />
+                <input type="text" name="tipo" value={produto.tipo} onChange={handleChange} />
             </div>
             <div>
                 <label>Email:</label>
-                <input type="email" name="email" value={cliente.email} onChange={handleChange} />
+                <input type="text" name="dataValidade" value={produto.dataValidade} onChange={handleChange} />
             </div>
             <div>
                 <label>Número de Telefone:</label>
-                <input type="text" name="numTelefone" value={cliente.numTelefone} onChange={handleChange} />
+                <input type="text" name="qtdMinima" value={produto.qtdMinima} onChange={handleChange} />
             </div>
             <div>
-                <label>Data de Nascimento:</label>
-                <input type="date" name="dataNascimento" value={cliente.dataNascimento} onChange={handleChange} />
+                <label>Quantidade:</label>
+                <input type="text" name="quantidade" value={produto.quantidade} onChange={handleChange} />
             </div>
-                <button type="submit">Salvar</button>
+            <div>
+                <label>Valor:</label>
+                <input type="text" name="valor" value={produto.valor} onChange={handleChange} />
+            </div>
+            <div>
+                <label>Status:</label>
+                <input type="text" name="status" value={produto.status} onChange={handleChange} />
+            </div>
+                <button type="submit">Salvar Informações</button>
         </form>
         </div>
     );
