4360dcabf6d578b2ed83dd8d076b1d8b1849b651
FelipeSouza14 <felipe.dev.2003@gmail.com>
2024-05-08 11:39:21 -0300

    add a primeira parte do CRUD Funcionário #70

---
+++ front_end/pages/employee/nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function NavEmployee() {
+    return (
+        <nav>
+            <h2>Navegação temporária até aplicar a estilização do CRUD - Funcionário</h2>
+            <div>
+                <Link href="createEmployee" passHref>
+                    Inserir Funcionário
+                </Link>
+            </div>
+            {/* <div>
+                <Link href="read" passHref>
+                    Consultar Cliente
+                </Link>
+            </div> */}
+        </nav>
+    );
+}
