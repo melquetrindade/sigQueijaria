5e196a9c5a1ca15fd30a14e24fd614168ea03665
Isa Kaillany <isakaillany123@gmail.com>
2024-05-12 19:02:46 -0300

    add estilização do create product #83

--- front_end/pages/product/create.js
+++ front_end/pages/product/create.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-export default function AddProduto () {
+export default function AddProduto() {
     const [produto, setProduto] = useState({
-        codigoBarras: '',
-        nome: '',
-        tipo: '',
-        dataValidade: '',
-        qtdMinima: '',
-        quantidade: '',
-        valor: '',
-        status: ''
+        codigoBarras: "",
+        nome: "",
+        tipo: "",
+        dataValidade: "",
+        qtdMinima: "",
+        quantidade: "",
+        valor: "",
+        status: "",
     });
 
     const handleChange = (e) => {
@@ -17,74 +17,145 @@ export default function AddProduto () {
         setProduto({ ...produto, [name]: value });
     };
 
-    const handleSubmit = async (e) => { // esta função envia os dados do produto para o back usando o método POST    
+    const handleSubmit = async (e) => {
+        // esta função envia os dados do produto para o back usando o método POST
         e.preventDefault();
-        
+
         try {
-            const response = await fetch('http://127.0.0.1:8000/produtos/', {
-                method: 'POST',
+            const response = await fetch("http://127.0.0.1:8000/produtos/", {
+                method: "POST",
                 headers: {
-                    'Content-Type': 'application/json',
+                    "Content-Type": "application/json",
                 },
                 body: JSON.stringify(produto),
             });
-    
+
             const data = await response.json(); // transforma a resposta em JSON
-            console.log(data); 
-    
+            console.log(data);
+
             setProduto({
-                codigoBarras: '',
-                nome: '',
-                tipo: '',
-                dataValidade: '',
-                qtdMinima: '',
-                quantidade: '',
-                valor: '',
-                status: ''
+                codigoBarras: "",
+                nome: "",
+                tipo: "",
+                dataValidade: "",
+                qtdMinima: "",
+                quantidade: "",
+                valor: "",
+                status: "",
             });
         } catch (error) {
-            console.error('Erro ao inserir produto!', error);
+            console.error("Erro ao inserir produto!", error);
         }
     };
-    
+
     return (
-        <div>
-        <h2>Inserir Produto</h2>
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>CPF:</label>
-                <input type="text" name="codigoBarras" value={produto.codigoBarras} onChange={handleChange} />
+        <div className="flex flex-col justify-center items-center h-2/4 my-4 max-w-full p-3">
+            <div className="shadow-lg shadow-slate-400 max-w-full">
+                <h1 className="w-full text-center p-[10px] bg-gradient-to-bl from-[#09173E] via-[#03475B] to-[#084D6E]/75 text-white font-bold text-2xl">
+                    Cadastrar Produto
+                </h1>
+                <form
+                    onSubmit={handleSubmit}
+                    className="bg-white px-6 py-5 flex flex-col w-[470px] max-w-full gap-2"
+                >
+                    <div className="flex flex-col justify-center gap-1">
+                        <label className="font-semibold">CPF:</label>
+                        <input
+                            className="border-0 border-b-2 shadow-sm shadow-slate-400"
+                            type="text"
+                            name="codigoBarras"
+                            value={produto.codigoBarras}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col justify-center gap-1">
+                        <label className="font-semibold">RG:</label>
+                        <input
+                            className="border-0 border-b-2 shadow-sm shadow-slate-400"
+                            type="text"
+                            name="nome"
+                            value={produto.nome}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col justify-center gap-1">
+                        <label className="font-semibold">Nome:</label>
+                        <input
+                            className="border-0 border-b-2 shadow-sm shadow-slate-400"
+                            type="text"
+                            name="tipo"
+                            value={produto.tipo}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col justify-center gap-1">
+                        <label className="font-semibold">Email:</label>
+                        <input
+                            className="border-0 border-b-2 shadow-sm shadow-slate-400"
+                            type="text"
+                            name="dataValidade"
+                            value={produto.dataValidade}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col justify-center gap-1">
+                        <label className="font-semibold">
+                            Número de Telefone:
+                        </label>
+                        <input
+                            className="border-0 border-b-2 shadow-sm shadow-slate-400"
+                            type="text"
+                            name="qtdMinima"
+                            value={produto.qtdMinima}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col justify-center gap-1">
+                        <label className="font-semibold">Quantidade:</label>
+                        <input
+                            className="border-0 border-b-2 shadow-sm shadow-slate-400"
+                            type="text"
+                            name="quantidade"
+                            value={produto.quantidade}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col justify-center gap-1">
+                        <label className="font-semibold">Valor:</label>
+                        <input
+                            className="border-0 border-b-2 shadow-sm shadow-slate-400"
+                            type="text"
+                            name="valor"
+                            value={produto.valor}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <div className="flex flex-col justify-center gap-1">
+                        <label className="font-semibold">Status:</label>
+                        <input
+                            className="border-0 border-b-2 shadow-sm shadow-slate-400"
+                            type="text"
+                            name="status"
+                            value={produto.status}
+                            onChange={handleChange}
+                            required
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="w-full py-2 px-4 bg-[#03475B] hover:bg-[#084D6E]/95 rounded-md shadow-lg text-white font-semibold transition duration-200 mt-5"
+                    >
+                        Salvar
+                    </button>
+                </form>
             </div>
-            <div>
-                <label>RG:</label>
-                <input type="text" name="nome" value={produto.nome} onChange={handleChange} />
-            </div>
-            <div>
-                <label>Nome:</label>
-                <input type="text" name="tipo" value={produto.tipo} onChange={handleChange} />
-            </div>
-            <div>
-                <label>Email:</label>
-                <input type="text" name="dataValidade" value={produto.dataValidade} onChange={handleChange} />
-            </div>
-            <div>
-                <label>Número de Telefone:</label>
-                <input type="text" name="qtdMinima" value={produto.qtdMinima} onChange={handleChange} />
-            </div>
-            <div>
-                <label>Quantidade:</label>
-                <input type="text" name="quantidade" value={produto.quantidade} onChange={handleChange} />
-            </div>
-            <div>
-                <label>Valor:</label>
-                <input type="text" name="valor" value={produto.valor} onChange={handleChange} />
-            </div>
-            <div>
-                <label>Status:</label>
-                <input type="text" name="status" value={produto.status} onChange={handleChange} />
-            </div>
-                <button type="submit">Salvar Informações</button>
-        </form>
         </div>
     );
-};
+}
