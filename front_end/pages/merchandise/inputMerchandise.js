4970cb881fb878f449aa172fa9155f5d32087239
FelipeSouza14 <felipe.dev.2003@gmail.com>
2024-05-17 15:31:53 -0300

    add a primeira parte de entrada de mercadoria #72

--- front_end/pages/merchandise/inputMerchandise.js
+++ front_end/pages/merchandise/inputMerchandise.js
@@ -1,8 +1,51 @@
-
+import React, { useState } from "react";
 
 export default function InputMerchandise() {
+    const [inputMerchandise, setInputMerchandise] = useState({
+        quantidade: "",
+        codigo: "",
+        fornecedor: "",
+        data: "",
+        valor: "",
+    });
 
-    return(
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setInputMerchandise({ ...inputMerchandise, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        // esta função envia os dados do inputMerchandise para o back usando o método POST
+        e.preventDefault();
+
+        try {
+            const response = await fetch(
+                "http://127.0.0.1:8000/entradasMercadorias/",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(inputMerchandise),
+                }
+            );
+
+            const data = await response.json(); // transforma a resposta em JSON
+            console.log(data);
+
+            setInputMerchandise({
+                quantidade: "",
+                codigo: "",
+                fornecedor: "",
+                data: "",
+                valor: "",
+            });
+        } catch (error) {
+            console.error("Erro ao inserir inputMerchandise!", error);
+        }
+    };
+
+    return (
         <div>
             <p>aaaa</p>
         </div>
