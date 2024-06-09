cf3091de0583554916e8451b8a5d6a55077fb6d7
FelipeSouza14 <felipe.dev.2003@gmail.com>
2024-06-09 17:00:28 -0300

    Modularizando o estoque #120

--- front_end/pages/merchandise/homePageStock.js
+++ front_end/pages/merchandise/homePageStock.js
@@ -1,9 +1,32 @@
+import Link from "next/link";
 
 
-export default function HomePageStock () {
-    return(
-        <div>
-            
+export default function HomePageStock({produtos}) {
+    return (
+        <div className="flex flex-col justify-center items-cente h-96 gap-20">
+            <Link>
+                <div
+                    className="bg-white font-bold shadow-md cursor-pointer p-4 w-52 rounded-xl 
+                        flex justify-evenly items-center"
+                >
+                    Novo Produto
+                </div>
+            </Link>
+            <div className="bg-slate-900 text-white">
+                {produtos.map((product) => (
+                    <div
+                        key={product.id}
+                        className="flex gap-5 justify-between items-center w-80 mb-5"
+                    >
+                        <div>
+                            {product.nome} ------ {product.quantidade}
+                        </div>
+                        <button onClick={() => setProductId(product.id)}>
+                            add +
+                        </button>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
---
+++ front_end/pages/merchandise/inputNewProduct.js
@@ -0,0 +1,7 @@
+export default function InputNewProduct () {
+    return(
+        <div>
+            
+        </div>
+    );
+}
