29a7df8e2d1eb35e53bfafdbc79df5369da60b37
Melque Rodrigues <melquetrindade654@gmail.com>
2024-07-17 17:24:30 -0300

    Implementando testes relacionados a autenticação de usuários #130

--- back_end/api/core/tests/test_views.py
+++ back_end/api/core/tests/test_views.py
@@ -3,22 +3,22 @@ from ..models import CustomUser
 
 class TestViews(TestSetUp):
     # teste para verficar que dá erro
-    '''
+    
     def teste_cliente_pode_registrar_sem_dados(self):
         res=self.client.post(self.register_url)
-        import pdb
-        pdb.set_trace()
+        #import pdb
+        #pdb.set_trace()
 
         self.assertEqual(res.status_code, 400)
-    '''
-    '''
+    
+    
     def teste_cliente_pode_registrar(self):
         res=self.client.post(self.register_url, self.user_data, format="json")
         #import pdb
         #pdb.set_trace()
 
         self.assertEqual(res.status_code, 201)
-    '''
+    
 
     def teste_usuario_nao_pode_fazer_login_com_email_invalido(self):
         response=self.client.post(self.register_url, self.user_data, format="json")
@@ -27,8 +27,6 @@ class TestViews(TestSetUp):
         user.is_verified = True
         user.save()
         res=self.client.post(self.login_url, self.user_data, format="json")
-        import pdb
-        pdb.set_trace()
         
 
         self.assertEqual(res.status_code, 200)
