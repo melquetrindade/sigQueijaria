e0ace195ab472b0c183a4e3825cf54613e62fa98
Isa Kaillany <isakaillany123@gmail.com>
2024-07-22 23:26:35 -0300

    criando testes para o crud de funcionário #134

--- back_end/api/core/tests/test_setup.py
+++ back_end/api/core/tests/test_setup.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 # Carrega variáveis de ambiente do arquivo .env
 load_dotenv()
 
+
 class TestSetUp(APITestCase):
 
     def setUp(self):
@@ -14,15 +15,16 @@ class TestSetUp(APITestCase):
         self.cliente_url = reverse("clientes-list")
         self.entradasMercadorias_url = reverse("entradasMercadorias-list")
         self.produtos_url = reverse("produtos-list")
+        self.funcionario_url = reverse("funcionarios-list")
 
-        self.user_data={
+        self.user_data = {
             "email": "maria@gmail.com",
             "username": "Maria",
             "password": os.getenv('PASSWORD'),
             "password2": os.getenv('PASSWORD2')
         }
 
-        self.client_data={
+        self.client_data = {
             "cpf": "12345678900",
             "rg": "111222333",
             "nome": "Maria",
@@ -31,14 +33,14 @@ class TestSetUp(APITestCase):
             "dataNascimento": "12/06/2002"
         }
 
-        self.merchandise_data={
+        self.merchandise_data = {
             "ownerFornecedor": 9,
             "quantidade": 300,
             "data": "22/07/2024",
             "valor": 600,
         }
 
-        self.product_data={
+        self.product_data = {
             "codigoBarras": "1231241232278",
             "tipo": "Branco",
             "nome": "Queijo de Ricota",
@@ -51,7 +53,18 @@ class TestSetUp(APITestCase):
             "valor": 250,
         }
 
+        self.employee_data = {
+            "cpf": "12345678900",
+            "cargo": "Caixa",
+            "salario": "25000",
+            "cargaHoraria": "8",
+            "nome": "João",
+            "email": "joao@gmail.com",
+            "numTelefone": "84998114363",
+            "dataNascimento": "12/06/2000"
+        }
+
         return super().setUp()
-    
+
     def tearDown(self):
         return super().tearDown()
