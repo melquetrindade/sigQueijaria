c8b237847237bcaa77a856c69ab865b1983af436
FelipeSouza14 <felipe.dev.2003@gmail.com>
2024-08-09 20:52:23 -0300

    Add scroll na tabela #168

--- front_end/pages/crudNav.js
+++ front_end/pages/crudNav.js
@@ -17,7 +17,7 @@ export default function CrudNav({ actions, placeholder, TableFunction, searchSta
                     </div>
                 ))}
             </div>
-            <div className="bg-white h-96 w-full flex flex-col rounded-xl shadow-md my-2 ">
+            <div className="bg-white h-96 w-full flex flex-col rounded-md shadow-md my-2 overflow-y-scroll">
                 <div className="flex justify-between items-center p-4 text-black">
                     <div className="flex justify-end items-center relative">
                         <input
