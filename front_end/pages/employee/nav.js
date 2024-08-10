7644c403184828c3d91d09af76ea0f52eb22e0d3
FelipeSouza14 <felipe.dev.2003@gmail.com>
2024-08-09 21:48:22 -0300

    Adicionando a tabela de funcionários #168

--- front_end/pages/employee/nav.js
+++ front_end/pages/employee/nav.js
@@ -3,8 +3,87 @@ import { RxUpdate } from "react-icons/rx";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { IoIosList } from "react-icons/io";
 import CrudNav from "../crudNav";
+import { useState, useEffect } from "react";
 
 export default function EmployeeCrud() {
+    const [employee, setEmployee] = useState([]);
+    const [search, setSearch] = useState("");
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const data = await fetch("http://127.0.0.1:8000/funcionarios/");
+                const dataEmployee = await data.json();
+                setEmployee(dataEmployee);
+            } catch (error) {
+                console.error("Erro ao carregar:", error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    const tHead = ["ID", "Nome", "CPF", "E-mail", "Cargo", "Salário", "Carga Horária", "Telefone", "Atividade"];
+    const Table = () => {
+        const filteredEmployee = employee.filter(
+            (employee) =>
+                employee.cpf.toLowerCase().includes(search.toLowerCase())
+        );
+
+        return (
+            <table className="w-full">
+                <thead>
+                    <tr>
+                        {tHead.map((tHead) => (
+                            <th className="bg-gray-100 border-b-2 border-gray-300 px-4 py-3 text-left text-gray-600 font-semibold">
+                                {tHead}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {filteredEmployee.map((employee, index) => (
+                        <tr key={index}
+                            className={
+                                index % 2 === 0
+                                    ? "bg-gray-50"
+                                    : "bg-gray-100"
+                            }
+                        >
+                            <td className="border-b border-gray-200 px-4 py-3 text-gray-700">
+                                {employee.id}
+                            </td>
+                            <td className="border-b border-gray-200 px-4 py-3 text-gray-700">
+                                {employee.nome}
+                            </td>
+                            <td className="border-b border-gray-200 px-4 py-3 text-gray-700">
+                                {employee.cpf}
+                            </td>
+                            <td className="border-b border-gray-200 px-4 py-3 text-gray-700">
+                                {employee.email}
+                            </td>
+                            <td className="border-b border-gray-200 px-4 py-3 text-gray-700">
+                                {employee.cargo}
+                            </td>
+                            <td className="border-b border-gray-200 px-4 py-3 text-gray-700">
+                                {employee.salario}
+                            </td>
+                            <td className="border-b border-gray-200 px-4 py-3 text-gray-700">
+                                {employee.cargaHoraria}
+                            </td>
+                            <td className="border-b border-gray-200 px-4 py-3 text-gray-700">
+                                {employee.numTelefone}
+                            </td>
+                            <td className="border-b border-gray-200 px-4 py-3 text-gray-700">
+                                {employee.status ? "Ativo" : "Inativo"}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        );
+    }
+    
     const actions = [
         { icon: FaPlus, label: "Novo Funcionário", color: "text-sky-400", onClick: () => window.location.href = "/employee/create"},
         { icon: IoIosList, label: "Consultar Dados", color: "text-[#01CBBF]", onClick: () => window.location.href = "/employee/read" },
@@ -12,6 +91,6 @@ export default function EmployeeCrud() {
         { icon: RiDeleteBin6Line, label: "Excluir Funcionário", color: "text-red-500", onClick: () => window.location.href = "/employee/delete" },
     ];
 
-    return <CrudNav actions={actions} placeholder="Digite o nome ou CNPJ..." />;
+    return <CrudNav actions={actions} placeholder="Digite o nome ou CNPJ..." TableFunction={Table} searchState={setSearch}/>;
 };
 
